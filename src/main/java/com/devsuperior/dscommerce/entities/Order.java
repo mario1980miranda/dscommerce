21cb34f9b2fb262bedcce07e537dc62df0744c0f
Mario Miranda <mario_chan15@hotmail.com>
2023-05-25 14:47:31 -0400

    From Order to getProducts and from Product to getOrders

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -23,27 +24,27 @@ import jakarta.persistence.Table;
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private OrderStatus status;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
-	
+
 	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		this.id = id;
 		this.moment = moment;
@@ -84,6 +85,22 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Product> getProducts() {
+		return this.items.stream().map(x -> x.getProduct()).toList();
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -105,5 +122,5 @@ public class Order implements Serializable {
 	public String toString() {
 		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + ", client=" + client + "]";
 	}
-	
+
 }
--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -89,4 +90,11 @@ public class Product {
 		return categories;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Order> getOrders() {
+		return this.items.stream().map(x -> x.getOrder()).toList();
+	}
 }
